298cc8a42d36ae0e6661d4dc0fdca22ed2ecfb25
SirJUST <theoperaking@yahoo.com>
2018-11-29 13:33:07 -0800

    player, anim, bg

---
+++ Assets/Scripts/BirdScript/BirdScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour {
+
+    public static BirdScript instance;
+
+    [SerializeField]
+    private Rigidbody2D myRigidBody;
+
+    [SerializeField]
+    private Animator anim;
+
+    private float forwardSpeed = 3f;
+
+    private float bounceSpeed = 4f;
+
+    private bool didFlap;
+
+    public bool isAlive;
+
+    void Awake()
+    {
+        if(instance== null)
+        {
+            instance = this;
+        }
+
+        isAlive = true;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (isAlive)
+        {
+            Vector3 temp = transform.position;
+            temp.x += forwardSpeed * Time.deltaTime;
+            transform.position = temp;
+
+            if (didFlap)
+            {
+                didFlap = false;
+                myRigidBody.velocity = new Vector2(0, bounceSpeed);
+                anim.SetTrigger("Flap");
+            }
+        }
+	}
+
+    public void FlapTheBird()
+    {
+        didFlap = true;
+    }
+}
